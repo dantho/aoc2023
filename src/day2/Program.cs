da8531e3a07dfa7752f6301bb2011c40218f3269
Dan Thornton <dan.thornton+Git@thornton.me>
2023-12-02 00:36:10 -0500

    Initialized Day 2

---
+++ src/day2/Program.cs
@@ -0,0 +1,47 @@
+﻿// https://adventofcode.com/2023/day/2
+using System.Diagnostics;
+using System;
+
+int aocPart = 2;
+string[] lines = System.IO.File.ReadAllLines(@"C:\Users\DanTh\github\aoc2023\inputs\day2.txt");
+
+if (aocPart == 1)
+    // Part 1 example input
+    lines = new string[] {
+    "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+    "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+    "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+    "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+    "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
+        };
+//else if (aocPart == 2)
+//    // Part 2 example input
+//    lines = new string[] {
+//        "two1nine",
+//        "eightwothree",
+//        "abcone2threexyz",
+//        "xtwone3four",
+//        "4nineeightseven2",
+//        "zoneight234",
+//        "7pqrstsixteen"
+//        };
+//else
+//    Debug.Assert(false, "aocPart should be 1 or 2");
+
+
+foreach (var line in lines)
+{
+    Console.WriteLine(line);
+}
+
+Debug.Assert(true);
+
+int sum = 0;
+Console.WriteLine($"Part {aocPart} answer is {sum}");
+
+struct Grab
+{
+    int redCount;
+    int blueCount;
+    int greenCount;
+};

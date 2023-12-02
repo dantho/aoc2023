7e6f6cc4bbd319e3de4169bc08a4933bf1a9dec6
Dan Thornton <dan.thornton+Git@thornton.me>
2023-12-02 00:14:47 -0500

    Day 1 done

---
+++ README.md
@@ -0,0 +1,33 @@
+# Advent of Code 2022
+
+![](pix/aoc.png)
+
+Teradyne private [Leaderboard](https://adventofcode.com/2023/leaderboard/private/view/380786) for [adventofcode.com/2023](https://adventofcode.com/2023)
+
+| Day | Stars | Comments (**SPOILER ALERT!**) |
+|---|:-:|-|
+| [1: Trebuchet?!](https://adventofcode.com/2023/day/1) |⭐⭐| Calculate Calibration values from fancy formatted input |
+| [2: ](https://adventofcode.com/2023/day/2) || (Not Yet Available) |
+| [3: ](https://adventofcode.com/2023/day/2) || (Not Yet Available) |
+| [4: ](https://adventofcode.com/2023/day/2) || (Not Yet Available) |
+| [5: ](https://adventofcode.com/2023/day/2) || (Not Yet Available) |
+| [6: ](https://adventofcode.com/2023/day/2) || (Not Yet Available) |
+| [7: ](https://adventofcode.com/2023/day/2) || (Not Yet Available) |
+| [8: ](https://adventofcode.com/2023/day/2) || (Not Yet Available) |
+| [9: ](https://adventofcode.com/2023/day/2) || (Not Yet Available) |
+| [10: ](https://adventofcode.com/2023/day/10) || (Not Yet Available) |
+| [11: ](https://adventofcode.com/2023/day/11) || (Not Yet Available) |
+| [12: ](https://adventofcode.com/2023/day/12) || (Not Yet Available) |
+| [13: ](https://adventofcode.com/2023/day/13) || (Not Yet Available) |
+| [14: ](https://adventofcode.com/2023/day/14) || (Not Yet Available) |
+| [15: ](https://adventofcode.com/2023/day/15) || (Not Yet Available) |
+| [16: ](https://adventofcode.com/2023/day/16) || (Not Yet Available) |
+| [17: ](https://adventofcode.com/2023/day/17) || (Not Yet Available) |
+| [18: ](https://adventofcode.com/2023/day/18) || (Not Yet Available) |
+| [19: ](https://adventofcode.com/2023/day/19) || (Not Yet Available) |
+| [20: ](https://adventofcode.com/2023/day/20) || (Not Yet Available) |
+| [21: ](https://adventofcode.com/2023/day/21) || (Not Yet Available) |
+| [22: ](https://adventofcode.com/2023/day/22) || (Not Yet Available) |
+| [23: ](https://adventofcode.com/2023/day/23) || (Not Yet Available) |
+| [24: ](https://adventofcode.com/2023/day/24) || (Not Yet Available) |
+| [25: ](https://adventofcode.com/2023/day/25) || (Not Yet Available) |
---
+++ src/day1/Program.cs
@@ -0,0 +1,87 @@
+﻿// https://adventofcode.com/2023/day/1
+using System.Diagnostics;
+using System;
+
+int aocPart = 2;
+string[] lines = System.IO.File.ReadAllLines(@"C:\Users\DanTh\github\aoc2023\inputs\day1.txt");
+string[] digitWords = { "zero_isnotallowed", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+List<List<int>> cals = new();
+
+//if (aocPart == 1)
+//    // Part 1 example input
+//    lines = new string[] {
+//        "1abc2",
+//        "pqr3stu8vwx",
+//        "a1b2c3d4e5f",
+//        "treb7uchet"
+//        };
+//else if (aocPart == 2)
+//    // Part 2 example input
+//    lines = new string[] {
+//        "two1nine",
+//        "eightwothree",
+//        "abcone2threexyz",
+//        "xtwone3four",
+//        "4nineeightseven2",
+//        "zoneight234",
+//        "7pqrstsixteen"
+//        };
+//else
+//    Debug.Assert(false, "aocPart should be 1 or 2");
+
+foreach (string rawline in lines)
+{
+    var line = rawline;
+    if (aocPart == 2)
+    {
+        int? firstNdx;
+        int firstPos;
+        do
+        {
+            firstNdx = null;
+            firstPos = int.MaxValue;
+            for (int i = 0; i <= 9; i++)
+            {
+                if (line.Contains(digitWords[i]))
+                {
+                    int pos = line.IndexOf(digitWords[i]);
+                    if (firstNdx == null || firstPos > pos)
+                    {
+                        firstNdx = i;
+                        firstPos = pos;
+                    }
+                }
+            }
+            // '.Length-1' at end is a hack to fix Part 2. Retain the last letter of each digit-word in case the next word is using that letter.
+            // so "twone" becomes "21" instead of "2ne".  Works!
+            if (firstNdx != null)
+                line = string.Concat(line[..firstPos], firstNdx.ToString(), line.AsSpan(firstPos + digitWords[(int)firstNdx].Length-1));
+        } while (firstNdx != null);
+    }
+    List<int> cal = new();
+    foreach (char c in line)
+    {
+        if (c >= '0' && c <= '9')
+        {
+            int num;
+            if (int.TryParse(c.ToString(), out num))
+                cal.Add(num);
+        }
+    }
+    Debug.Assert(cal.Count > 0, $"Empty list of number found for {line}");
+    cals.Add(cal);
+}
+
+int sum = 0;
+foreach (var cal in cals)
+{
+    int value = cal.First() * 10 + cal.Last();
+    sum += value;
+    foreach (var n in cal)
+        Console.Write(n);
+    Console.WriteLine($" -> {value}");
+}
+
+Debug.Assert(cals.Count == lines.Length);
+
+Console.WriteLine($"Part {aocPart} answer is {sum}");

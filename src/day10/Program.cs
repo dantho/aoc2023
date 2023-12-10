ed2b1af72d3bb2f850ad23b4e50964ccc74fcee0
Dan Thornton <dan.thornton+Git@thornton.me>
2023-12-10 01:31:59 -0500

    Day 10 WIP

--- src/day10/Program.cs
+++ src/day10/Program.cs
@@ -8,6 +8,14 @@ using System.Xml.Schema;
 
 int aocPart = 1;
 string rawInput = System.IO.File.ReadAllText(@"C:\Users\DanTh\github\aoc2023\inputs\day10.txt");
+
+// Example 1 input
+rawInput = @"..F7.
+.FJ|.
+SJ.L7
+|F--J
+LJ...";
+
 //https://en.wikipedia.org/wiki/Box-drawing_character
 string boxChars = "┓┏┛┗┃━";
 string boxInput = string.Concat(rawInput.Select(c =>
@@ -25,19 +33,10 @@ string boxInput = string.Concat(rawInput.Select(c =>
 }));
 
 System.IO.File.WriteAllText(@"C:\Users\DanTh\github\aoc2023\out\day10_out.txt", boxInput);
-// // Example 1 input
-// lines = new string[] {
-//     "bla bla ...",
-//     };
 
-// // Example 2 input
-// lines = new string[] {
-//     "bla bla ...",
-//     };
-//foreach (var line in lines)
-//{
-//    Console.WriteLine(line);
-//}
+string[] lines = boxInput.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+char[][] grid = lines.Select(s => s.ToArray()).ToArray();
 
 int ansPart1 = 0;
 int ansPart2 = 0;

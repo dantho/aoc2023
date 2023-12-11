05c92779324e10843b17f1debb36d02b1737baa2
Dan Thornton <dan.thornton+Git@thornton.me>
2023-12-11 18:49:56 -0500

    Day 12 prep

---
+++ src/day12/Program.cs
@@ -0,0 +1,37 @@
+﻿// https://adventofcode.com/2023/day/12
+using System.Diagnostics;
+using System;
+using System.Text.RegularExpressions;
+using System.Drawing;
+using System.ComponentModel.Design;
+using System.Xml.Schema;
+
+int aocPart = 1;
+string[] lines = System.IO.File.ReadAllLines(@"C:\Users\DanTh\github\aoc2023\inputs\day12.txt");
+
+// // Example 1 input
+// lines = new string[] {
+//     "bla bla ...",
+//     };
+
+// // Example 2 input
+// lines = new string[] {
+//     "bla bla ...",
+//     };
+
+foreach (var line in lines)
+{
+    Console.WriteLine(line);
+}
+
+int ansPart1 = 0;
+int ansPart2 = 0;
+
+Console.WriteLine($"The answer for Part {1} is {ansPart1}");
+Console.WriteLine($"The answer for Part {2} is {ansPart2}");
+
+Console.ReadKey();
+
+// End
+// End
+// End

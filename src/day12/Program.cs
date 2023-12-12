9db0532ef19c7db2e435c270b3f7374392437ecd
Dan Thornton <dan.thornton+Git@thornton.me>
2023-12-12 18:57:57 -0500

    Day 12 WIP

--- src/day12/Program.cs
+++ src/day12/Program.cs
@@ -9,15 +9,28 @@ using System.Xml.Schema;
 int aocPart = 1;
 string[] lines = System.IO.File.ReadAllLines(@"C:\Users\DanTh\github\aoc2023\inputs\day12.txt");
 
-// // Example 1 input
-// lines = new string[] {
-//     "bla bla ...",
-//     };
+ // Example 1 input
+ lines = new string[]
+ {
+    "???.### 1,1,3",
+    ".??..??...?##. 1,1,3",
+    "?#?#?#?#?#?#?#? 1,3,1,6",
+    "????.#...#... 4,1,1",
+    "????.######..#####. 1,6,5",
+    "?###???????? 3,2,1"
+};
 
 // // Example 2 input
 // lines = new string[] {
 //     "bla bla ...",
 //     };
+
+string[][] rawStatus = lines.Select(line =>
+    line.Split(' ', StringSplitOptions.RemoveEmptyEntries)).ToArray();
+(string, int[])[] status = rawStatus.Select(halves =>
+    (halves[0], halves[1].Split(',').Select(int.Parse).ToArray())).ToArray();
+Console.WriteLine(status[0].Item1);
+Console.WriteLine(status[0].Item2[2]);
 
 foreach (var line in lines)
 {

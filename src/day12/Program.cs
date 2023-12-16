4e463e7e15e00797e71eb563eddc8a5772bc77ed
Dan Thornton <dan.thornton+Git@thornton.me>
2023-12-15 22:25:49 -0500

    Day12 more refactoring WIP

--- src/day12/Program.cs
+++ src/day12/Program.cs
@@ -10,16 +10,16 @@ using System.Runtime.CompilerServices;
 int aocPart = 1;
 string[] lines = System.IO.File.ReadAllLines(@"C:\Users\DanTh\github\aoc2023\inputs\day12.txt");
 
-//// Example input
-//lines = new string[]
-//{
-//    "???.### 1,1,3",
-//    ".??..??...?##. 1,1,3",
-//    "?#?#?#?#?#?#?#? 1,3,1,6",
-//    "????.#...#... 4,1,1",
-//    "????.######..#####. 1,6,5",
-//    "?###???????? 3,2,1"
-//};
+// Example input
+lines = new string[]
+{
+    "???.### 1,1,3",
+    ".??..??...?##. 1,1,3",
+    "?#?#?#?#?#?#?#? 1,3,1,6",
+    "????.#...#... 4,1,1",
+    "????.######..#####. 1,6,5",
+    "?###???????? 3,2,1"
+};
 
 // Extract into 2 strings
 string[][] rawStatus = lines.Select(line =>
@@ -29,7 +29,14 @@ string[][] rawStatus = lines.Select(line =>
     (half[0], half[1].Split(',').Select(int.Parse).Reverse().ToArray())).ToArray();
 // Now consume the string one chunk at a time, based on the first integer, and recurse
 // with remainder of string and remainder of ints
+
 int ansPart1 = 0;
+foreach ((string wildcardSprings, int[] validationRecords) in wildStatus)
+{
+    var validRecords = validationRecords.ToList();
+    string wildcardSpringsReversed = new string(wildcardSprings.Reverse().ToArray());
+    ansPart1 += PossibleValidArrangementCount(wildcardSpringsReversed, validRecords);
+}
 
 if (ansPart1 > 1000)
     Debug.Assert(ansPart1 == 8193);
@@ -58,11 +65,11 @@ Console.ReadKey();
 // End
 // End
 
-int CountOfPossibleValidPatterns(string springs, List<int> validationData)
+int PossibleValidArrangementCount(string springs, List<int> validationData)
 {
     // '#' == known spring (so _must_ be used)
     // '.' == known empty spot (so _cannot_ be used)
-    // '?' == could be either (so can be used)
+    // '?' == could be either (so can be used or not)
     //
     // Process the right side of the string in the following recursive fashion
     // Eliminate chars that cannot match the size of the first validationData element.
@@ -75,8 +82,12 @@ int CountOfPossibleValidPatterns(string springs, List<int> validationData)
     // Terminate the recursion with the multiplicative identity (1) or cancelation (0)
     if (validationData.Count == 0)
     {
-        bool anySpringsRemaining = springs.IndexOf('#') >= 0;
-        return anySpringsRemaining ? 0 : 1;
+        bool noSpringsRemain = springs.IndexOf('#') < 0;
+        return noSpringsRemain ? 1 : 0;
+    }
+    if (springs.Length == 0)
+    {
+        return validationData.Count == 0 ? 1 : 0;
     }
 
     // Grab the validation-record we're working with on this iteration
@@ -91,22 +102,24 @@ int CountOfPossibleValidPatterns(string springs, List<int> validationData)
     }
     springs = springs.Substring(ndx);
 
+    // Reindex pointers
     int firstEmpty = springs.IndexOf('.');
     int firstSpring = springs.IndexOf('#');
 
     // Let's also trim an initial region that doesn't fit our vData
-    // Or terminate if such a region includes a spring (known/required, but cannot fit)
+    // Or terminate if such a region includes a spring (known/required)
+    // aka "If it doesn't fit, you must acquit."
     if (firstEmpty >= 0 && firstEmpty + 1 < vRecord)
     {
         if (firstSpring >= 0 && firstSpring < firstEmpty)
         {
-            return 0; // Invalid
+            return 0; // "If it doesn't fit, you must acquit."
         }
         else
         {
             int newStart = Math.Min(firstEmpty + 1, springs.Length - 1);
             // We'll recurse after trimming to get back to the top to start over
-            return CountOfPossibleValidPatterns(springs.Substring(newStart), validationData);
+            return PossibleValidArrangementCount(springs.Substring(newStart), validationData);
         }
     }
 
@@ -120,23 +133,37 @@ int CountOfPossibleValidPatterns(string springs, List<int> validationData)
 
     // Does an anchor limit the size?
     if (firstSpring >= 0 && firstSpring + vRecord - 1 < maxNdx)
-    {
         maxNdx = firstSpring + vRecord - 1;
-        if (springs[maxNdx + 1] == '#') maxNdx--;
-    }
+
     if (maxNdx - minNdx + 1 < vRecord)
         return 0; // Invalid
 
-    // Find all known springs in this region, we'll use the locations to reject adjacencies.
-    // Inclusion is OK, adjacencies will EXTEND our field past the validation record length
+    // Find all known springs in this region, we'll use these locations to reject adjacencies.
+    // Overlap of a spring is OK, but adjacencies will EXTEND
+    // our field past the validated record length which is not
     string region = springs.Substring(minNdx, maxNdx - minNdx + 1);
     List<int> springNdxes = region.IndicesOf('#');
 
-    // FINALLY, we can slide our vRecord length candidate-generator
-    // validate candidates here and know against adjacent springs
+    // FINALLY, we can slide our vRecord-length candidate through our region
+    // validating candidates don't have against adjacent springs
+    // (right side checking is all that is required, due to left-side anchoring on first spring)
     // then recurse further for each success,
     // adding the results form each to establish the return value.
     // Phew!
+    validationData.RemoveAt(0); // We're done processing this vRecord, remove it before we recurse.
+    int returnVal = 0;
+    for (int start = minNdx; start <= maxNdx-vRecord+1; start++)
+    {
+        int end = start + vRecord - 1;
+        if (end+1==springs.Length || springs[end+1] != '#')
+        {
+            returnVal +=
+                end + 1 < springs.Length ?
+                PossibleValidArrangementCount(springs[(end + 1)..], validationData) :
+                PossibleValidArrangementCount("", validationData);
+        }
+    }
+    return returnVal;
 }
 
 public static class MyExtensions
@@ -145,7 +172,7 @@ public static class MyExtensions
     {
         int ndx = input.IndexOf(searchChar);
         if (ndx < 0) return new();
-        var indices = input[(ndx + 1)..].IndicesOf(searchChar);
+        var indices = input[(ndx + 1)..].IndicesOf(searchChar).Select(i => i + ndx + 1).ToList();
         indices.Add(ndx);
         return indices;
     }
